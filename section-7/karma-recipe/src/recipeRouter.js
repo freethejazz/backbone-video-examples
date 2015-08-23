9ba83ac2af30ed0a511e1c08d2baf6fa6add373e
Jonathan Freeman <freethejazz@gmail.com>
2015-08-23 12:32:31 -0500

    Add section 7 example code

---
+++ section-7/karma-recipe/src/recipeRouter.js
@@ -0,0 +1,15 @@
+var app = window.app || {};
+app.Routers = app.Routers || {};
+
+app.Routers.RecipeRouter = Backbone.Router.extend({
+  routes: {
+    'recipes/:id': 'openRecipe',
+    'recipe/:id/variation': 'createVariation'
+  },
+  openRecipe: function() {
+
+  },
+  createVariation: function () {
+
+  }
+});
