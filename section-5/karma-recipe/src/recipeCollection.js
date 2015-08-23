720c09556e2340099dcf934321601f4ea020ffd8
Jonathan Freeman <freethejazz@gmail.com>
2015-08-23 12:14:10 -0500

    Add section 5 example code

---
+++ section-5/karma-recipe/src/recipeCollection.js
@@ -0,0 +1,19 @@
+var app = window.app || {};
+app.Collections = app.Collections || {};
+
+app.Collections.Recipes = Backbone.Collection.extend({
+  model: app.Models.Recipe,
+  comparator: 'dateAdded',
+  url: '/recipes',
+  getRecipesNames: function() {
+    return _.uniq(this.pluck('name'));
+  },
+  getNewestForName: function(recipeName) {
+    var variations = this.where({
+      name: recipeName
+    });
+
+    return _.last(variations);
+  }
+});
+
