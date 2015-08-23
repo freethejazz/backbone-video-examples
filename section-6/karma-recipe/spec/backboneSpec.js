422f3d57c3fa5db11c0a7572ca22efd30cfc934b
Jonathan Freeman <freethejazz@gmail.com>
2015-08-23 12:27:57 -0500

    Add section 6 example code

---
+++ section-6/karma-recipe/spec/backboneSpec.js
@@ -0,0 +1,13 @@
+describe('A page with Backbone', function() {
+  it('should have access to Underscore', function() {
+    expect(window._).toBeDefined();
+    expect(window._.VERSION).toBeDefined();
+  });
+
+  it('should have access to Backbone', function() {
+    expect(window.Backbone).toBeDefined();
+    expect(window.Backbone.Model).toBeDefined();
+    expect(window.Backbone.Collection).toBeDefined();
+    expect(window.Backbone.View).toBeDefined();
+  });
+});
